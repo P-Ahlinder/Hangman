2cfd826a7d81fb52ee08cc285a84aaafc4ac9e14
Per Ahlinder <ahlinder1990@gmail.com>
2023-01-24 11:22:30 +0100

    fixed bugs with program, seems to be working fine for now

--- HangmanTest/UnitTest1.cs
+++ HangmanTest/UnitTest1.cs
@@ -1,10 +1,27 @@
+using Hangman.Components;
 namespace HangmanTest;
+
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
+        int result = 0;
 
+        result = 2 + 2;
+        Assert.Equal(4, result);
+        Console.WriteLine(result);
     }
+
+    [Fact]
+    public void Test2()
+    {
+        int result = 0;
+
+        result = 4;
+        Assert.Equal(4, result);
+        Console.WriteLine(result);
+    }
+
 }
